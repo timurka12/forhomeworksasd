51b708159bea7c6fcdb86c40eb1c6ccce9bf1709
timurka12 <93543993+timurka12@users.noreply.github.com>
2021-12-19 20:33:26 +0300

    Add files via upload

---
+++ Program24.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            HashSet<string> Hash = new HashSet<string>();
+            while (Hash.Count < 10_000)
+            {
+                string value = default;
+                for (int j = 0; j < r.Next(5, 10); j++) value += ((char)r.Next('a', 'z')).ToString();
+                Hash.Add(value);
+            }
+            Stopwatch stop = Stopwatch.StartNew();
+            foreach (var item in Hash)
+            {
+                if (item.Equals("балдееееж"))
+                {
+                    break;
+                }
+            }
+            stop.Stop();
+            TimeSpan ts = stop.Elapsed;
+            string time = $"{ts.Hours}:{ts.Minutes}:{ts.Seconds}:{ts.Milliseconds / 10}";
+            Console.WriteLine("RunTime " + time);
+        }
+    }
+}
